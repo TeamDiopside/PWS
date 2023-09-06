9ced75ada51ee0cb414047f0003da994a05fb661
LarsMans <l.s.vanemmerik@gmail.com>
2023-09-06 15:33:06 +0200

    start

---
+++ src/main/java/nl/teamdiopside/pws/Network.java
@@ -0,0 +1,19 @@
+package nl.teamdiopside.pws;
+
+public class Network {
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+/*
+ * voor elke laag is er een matrix
+ * een vierkante voor de weights
+ *  - rijen: alle weights die naar 1 neuron gaan
+ *  - kolommen: weights van alle neurons
+ * keer een vector voor alle komende neurons
+ * plus een vector voor de weight van elk komend neuron
+ *
+ */
---
+++ src/main/java/nl/teamdiopside/pws/Neuron.java
@@ -0,0 +1,11 @@
+package nl.teamdiopside.pws;
+
+import java.util.Vector;
+
+public class Neuron {
+
+    public Vector<Float> inputs;
+
+    public Neuron() {
+    }
+}
