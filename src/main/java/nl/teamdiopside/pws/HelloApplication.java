bfc71a818495c09ba6e1e4e6ac91ee9ee67d818f
Curry <66020230+curryducker@users.noreply.github.com>
2023-09-06 16:09:28 +0200

    function generation deel 1 voor parcours

--- src/main/java/nl/teamdiopside/pws/HelloApplication.java
+++ src/main/java/nl/teamdiopside/pws/HelloApplication.java
@@ -1,20 +1,52 @@
 package nl.teamdiopside.pws;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+        Pane pane = new Pane();
+
+        // Pat
+        Path path = new Path();
+        path.setStroke(Color.BLUE); // Stroke color
+
+        // Wat doet grafriek
+        double startX = 1;
+        double endX = 100;
+        double increment = 0.1;
+
+        for (double x = startX; x <= endX; x += increment) {
+
+            double y = function(x);
+
+            // Hier moeten de coordinaten op het scherm passen, dat werkt nog niet
+            double screenX = x * 16 + 100;
+            // -9 want y werkt van boven naar beneden en dat willen we niet
+            double screenY = y * -9 + 100;
+
+            // maak lijntje
+            if (x == startX) {
+                path.getElements().add(new MoveTo(screenX, screenY));
+            } else {
+                path.getElements().add(new LineTo(screenX, screenY));
+            }
+        }
+
+        pane.getChildren().add(path);
+        Scene scene = new Scene(pane, 1920, 1080);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Tytol");
+        primaryStage.show();
+    }
+
+    public double function(double x) {
+        return x*x;
     }
 
     public static void main(String[] args) {
