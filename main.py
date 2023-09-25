50b16f0886cb03d6cafe62d009f920382839b14e
LarsMans <l.s.vanemmerik@gmail.com>
2023-09-25 14:47:44 +0200

    comments

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import math
 import random
 
-import numpy as np
+import numpy
 
 
 def main():
+    # alle hidden layers op volgorde, eerste is input, laatste is output, getallen = aantal nodes in layer
     layers = [2, 3, 4, 1]
 
-    all_weights = [0] * (len(layers) - 1)
+    # lijst voor alle weight matrices
+    all_weights: list = [0] * (len(layers) - 1)
 
-    # maken van weights matrices, nu nog compleet random vulling
+    # maken van weights matrices
     for x in range(len(all_weights)):
         weights = create_random_matrix(layers[x + 1], layers[x])
         all_weights[x] = weights
@@ -18,24 +20,25 @@ def main():
     inputs = [0.3, 0.2]
     print(inputs)
     for weight in all_weights:
-        inputs = np.matmul(weight, inputs)
+        inputs = numpy.matmul(weight, inputs)  # voor elke weight matrix de nieuwe inputs berekenen
         for x in range(len(inputs)):
-            inputs[x] = sigmoid(inputs[x])
+            inputs[x] = sigmoid(inputs[x])  # alles door de sigmoid functie halen
         print(inputs)
-    print(inputs[0] - 0.5)
+    print(inputs[0])
 
 
 def create_random_matrix(height, width):
-    matrix = []
+    matrix = []  # lege matrix maken
     for x in range(height):
-        lijst = []
+        lijst = []  # voor elke rij van de matrix een nieuwe lijst maken (voor de kolom)
         for y in range(width):
-            lijst.append(random.random())
-        matrix.append(lijst)
+            lijst.append(random.random())  # de lijst vullen met random nummers
+        matrix.append(lijst)  # de lijst in de matrix doen
 
     return matrix
 
 
+# niemand die weet waarom dit zo is
 def sigmoid(x):
     return 1 / (1 + math.exp(-x))
 
